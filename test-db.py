ff9cad2d9d0f0e40e3f05c4c551e43b26d25cd67
Alex Manusovich <manusovich@gmail.com>
2015-08-16 16:56:18 -0500

    wip

--- test-db.py
+++ test-db.py
@@ -4,7 +4,7 @@ from pygame.locals import *
 from dataprovider import DataProvider
 
 data = DataProvider()
-all_records = data.all("Alex")
+all_records = data.all('Alex')
 print all_records
 
 count = sum(1 for r in all_records)
