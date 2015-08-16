258b58f7a61050f1fa8452347310cc9044fdb870
Alex Manusovich <manusovich@gmail.com>
2015-08-16 17:09:22 -0500

    wip

--- test-db.py
+++ test-db.py
@@ -3,8 +3,10 @@
 from pygame.locals import *
 from dataprovider import WeightRecord, DataProvider
 
+DataProvider.db_path = "/home/pi/weight_db"
+
 data = DataProvider()
-all_records = data.last("Alex")
+all_records = data.get_db().filter(WeightRecord, {'user': 'Alex'})
 print all_records
 
 count = sum(1 for r in all_records)
